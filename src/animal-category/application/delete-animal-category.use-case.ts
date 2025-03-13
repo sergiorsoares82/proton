60dabb3f7fe052029f7d48b81528ef7c2a9f0852
sergiorsoares82 <sergio.veiga.soares@gmail.com>
2025-03-13 22:38:32 +0000

    Create animal category delete use case

---
+++ src/animal-category/application/delete-animal-category.use-case.ts
@@ -0,0 +1,22 @@
+import type { IUseCase } from "../../shared/application/use-case.interface";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import type { IAnimalCategoryRepository } from "../domain/animal-category.repository";
+
+export class DeleteAnimalCategoryUseCase
+  implements IUseCase<DeleteAnimalCategoryInput, DeleteAnimalCategoryOutput>
+{
+  constructor(private readonly animalCategoryRepo: IAnimalCategoryRepository) {}
+
+  async execute(
+    input: DeleteAnimalCategoryInput
+  ): Promise<DeleteAnimalCategoryOutput> {
+    const uuid = new Uuid(input.animalCategoryId);
+    await this.animalCategoryRepo.delete(uuid);
+  }
+}
+
+export type DeleteAnimalCategoryInput = {
+  animalCategoryId: string;
+};
+
+type DeleteAnimalCategoryOutput = void;
