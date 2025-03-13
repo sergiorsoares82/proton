103910c20fca6974ec3093456e425a8dd01a3b81
sergiorsoares82 <sergio.veiga.soares@gmail.com>
2025-03-13 22:45:07 +0000

    Create test for animal category delete use case

---
+++ src/animal-category/application/__tests__/integration/delete-animal-category.use-case.int-spec.ts
@@ -0,0 +1,41 @@
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
+import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
+import { setupSequelize } from "../../../../shared/infra/testing/helpers";
+import { AnimalCategory } from "../../../domain/animal-category.aggregate";
+import { Gender } from "../../../domain/animal.aggregate";
+import { AnimalCategorySequelizeRepository } from "../../../infra/db/sequelize/animal-category-sequelize.repository";
+import { AnimalCategoryModel } from "../../../infra/db/sequelize/animal-category.model";
+import { DeleteAnimalCategoryUseCase } from "../../delete-animal-category.use-case";
+import { UpdateAnimalCategoryUseCase } from "../../update-animal-category.use-case";
+
+describe("Delete Animal Category Use Case Integration Tests", () => {
+  let useCase: DeleteAnimalCategoryUseCase;
+  let repository: AnimalCategorySequelizeRepository;
+
+  setupSequelize({ models: [AnimalCategoryModel] });
+
+  beforeEach(() => {
+    repository = new AnimalCategorySequelizeRepository(AnimalCategoryModel);
+    useCase = new DeleteAnimalCategoryUseCase(repository);
+  });
+
+  it("should throw an error on update when animal category is not found", async () => {
+    const uuid = new Uuid();
+
+    await expect(
+      useCase.execute({ animalCategoryId: uuid.id })
+    ).rejects.toThrow(new NotFoundError(uuid.id, AnimalCategory));
+  });
+
+  it("should delete an animal category", async () => {
+    const category = AnimalCategory.fake().aAnimalCategory().build();
+    await repository.insert(category);
+    await useCase.execute({
+      animalCategoryId: category.animalCategoryId.id,
+    });
+
+    await expect(
+      repository.findById(category.animalCategoryId)
+    ).resolves.toBeNull();
+  });
+});
---
+++ src/animal-category/application/__tests__/unit/delete-animal-category.use-case.spec.ts
@@ -0,0 +1,46 @@
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
+import {
+  InvalidUuidError,
+  Uuid,
+} from "../../../../shared/domain/value-objects/uuid.vo";
+import { setupSequelize } from "../../../../shared/infra/testing/helpers";
+import { AnimalCategory } from "../../../domain/animal-category.aggregate";
+import { Gender } from "../../../domain/animal.aggregate";
+import { AnimalCategoryInMemoryRepository } from "../../../infra/db/in-memory/animal-category-in-memory.repository";
+import { AnimalCategoryModel } from "../../../infra/db/sequelize/animal-category.model";
+import { DeleteAnimalCategoryUseCase } from "../../delete-animal-category.use-case";
+
+describe("Delete Animal Category Use Case Unit Test", () => {
+  let useCase: DeleteAnimalCategoryUseCase;
+  let repository: AnimalCategoryInMemoryRepository;
+
+  setupSequelize({ models: [AnimalCategoryModel] });
+
+  beforeEach(() => {
+    repository = new AnimalCategoryInMemoryRepository();
+    useCase = new DeleteAnimalCategoryUseCase(repository);
+  });
+
+  it("should throw an error on delete when animal category is not found", async () => {
+    await expect(() =>
+      useCase.execute({ animalCategoryId: "fake id" })
+    ).rejects.toThrow(new InvalidUuidError());
+
+    const uuid = new Uuid();
+
+    await expect(() =>
+      useCase.execute({ animalCategoryId: uuid.id })
+    ).rejects.toThrow(new NotFoundError(uuid.id, AnimalCategory));
+  });
+
+  it("should delte an animal category", async () => {
+    const items = [
+      new AnimalCategory({ name: "test 1", gender: Gender.FEMALE }),
+    ];
+    repository.items = items;
+    await useCase.execute({
+      animalCategoryId: items[0].animalCategoryId.id,
+    });
+    expect(repository.items).toHaveLength(0);
+  });
+});
