c3f25d2d9a8ee17072d00d87ce0a58166c535aa8
sergiorsoares82 <sergio.veiga.soares@gmail.com>
2025-03-13 01:45:27 +0000

    Create animal category insert use case

---
+++ src/animal-category/application/create-animal-category.use-case.ts
@@ -0,0 +1,40 @@
+import type { IUseCase } from "../../shared/application/use-case.interface";
+import { AnimalCategory } from "../domain/animal-category.aggregate";
+import type { IAnimalCategoryRepository } from "../domain/animal-category.repository";
+import type { Gender } from "../domain/animal.aggregate";
+
+export class CreateAnimalCategoryUseCase
+  implements IUseCase<CreateAnimalCategoryInput, CreateAnimalCategoryOutput>
+{
+  constructor(private readonly animalCategoryRepo: IAnimalCategoryRepository) {}
+
+  async execute(
+    input: CreateAnimalCategoryInput
+  ): Promise<CreateAnimalCategoryOutput> {
+    const entity = AnimalCategory.create(input);
+
+    await this.animalCategoryRepo.insert(entity);
+
+    return {
+      animalCategoryId: entity.animalCategoryId.id,
+      name: entity.name,
+      gender: entity.gender,
+      isActive: entity.isActive,
+      createdAt: entity.createdAt,
+    };
+  }
+}
+
+export type CreateAnimalCategoryInput = {
+  name: string;
+  gender: Gender;
+  isActive: boolean;
+};
+
+export type CreateAnimalCategoryOutput = {
+  animalCategoryId: string;
+  name: string;
+  gender: Gender;
+  isActive: boolean;
+  createdAt: Date;
+};
